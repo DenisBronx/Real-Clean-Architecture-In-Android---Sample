b872b1f745564bf0b86a28bbb7ef3e8f34db5950
Denis <dnsbrnd@gmail.com>
2024-10-16 16:52:20 +0100

    Extracted more functions

--- main-ui/src/main/java/com/denisbrandi/androidrealca/main/presentation/view/MainScreen.kt
+++ main-ui/src/main/java/com/denisbrandi/androidrealca/main/presentation/view/MainScreen.kt
@@ -13,9 +13,10 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.*
 import com.denisbrandi.androidrealca.designsystem.bottomNavElevation
-import com.denisbrandi.androidrealca.main.presentation.viewmodel.MainViewModel
+import com.denisbrandi.androidrealca.main.presentation.viewmodel.*
 import com.denisbrandi.androidrealca.main.ui.R
 import kotlinx.serialization.Serializable
 
@@ -52,44 +53,13 @@ internal fun MainScreen(
     makeWishlistScreen: @Composable () -> Unit,
     makeCartScreen: @Composable () -> Unit
 ) {
-    val state by mainViewModel.state.collectAsState()
-    var navigationSelectedItem by rememberSaveable {
-        mutableIntStateOf(0)
-    }
     val navController = rememberNavController()
     val topLevelRoutes = topLevelRoutes()
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = { Box(Modifier.size(0.dp)) },
         bottomBar = {
-            NavigationBar(
-                modifier = Modifier.shadow(elevation = bottomNavElevation)
-            ) {
-                topLevelRoutes.forEachIndexed { index, navigationItem ->
-                    NavigationBarItem(
-                        icon = {
-                            BottomBarIcon(
-                                index,
-                                state.wishlistBadge,
-                                state.cartBadge,
-                                navigationItem
-                            )
-                        },
-                        label = { Text(navigationItem.name) },
-                        selected = index == navigationSelectedItem,
-                        onClick = {
-                            navigationSelectedItem = index
-                            navController.navigate(navigationItem.route) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
-                                }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        }
-                    )
-                }
-            }
+            BottomNavigationBar(mainViewModel, topLevelRoutes, navController)
         }
     ) { paddingValues ->
         NavHost(
@@ -97,17 +67,68 @@ internal fun MainScreen(
             startDestination = topLevelRoutes.first().route,
             modifier = Modifier.padding(paddingValues = paddingValues)
         ) {
-            composable<NavProducts> {
-                makePLPScreen()
-            }
-            composable<NavWishlist> {
-                makeWishlistScreen()
-            }
-            composable<NavCart> {
-                makeCartScreen()
-            }
+            composable<NavProducts> { makePLPScreen() }
+            composable<NavWishlist> { makeWishlistScreen() }
+            composable<NavCart> { makeCartScreen() }
         }
     }
+}
+
+@Composable
+private fun BottomNavigationBar(
+    mainViewModel: MainViewModel,
+    topLevelRoutes: List<TopLevelRoute<Any>>,
+    navController: NavHostController
+) {
+    val state by mainViewModel.state.collectAsState()
+    val navigationSelectedItem = rememberSaveable {
+        mutableIntStateOf(0)
+    }
+    NavigationBar(
+        modifier = Modifier.shadow(elevation = bottomNavElevation)
+    ) {
+        topLevelRoutes.forEachIndexed { index, navigationItem ->
+            NavigationItem(
+                index,
+                state,
+                navigationItem,
+                navigationSelectedItem,
+                navController
+            )
+        }
+    }
+}
+
+@Composable
+private fun RowScope.NavigationItem(
+    index: Int,
+    state: MainState,
+    navigationItem: TopLevelRoute<Any>,
+    navigationSelectedItem: MutableState<Int>,
+    navController: NavHostController
+) {
+    NavigationBarItem(
+        icon = {
+            BottomBarIcon(
+                index,
+                state.wishlistBadge,
+                state.cartBadge,
+                navigationItem
+            )
+        },
+        label = { Text(navigationItem.name) },
+        selected = index == navigationSelectedItem.value,
+        onClick = {
+            navigationSelectedItem.value = index
+            navController.navigate(navigationItem.route) {
+                popUpTo(navController.graph.findStartDestination().id) {
+                    saveState = true
+                }
+                launchSingleTop = true
+                restoreState = true
+            }
+        }
+    )
 }
 
 @Composable
