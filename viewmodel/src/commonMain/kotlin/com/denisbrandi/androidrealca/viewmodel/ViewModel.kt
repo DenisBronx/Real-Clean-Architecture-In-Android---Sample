d37f1d1c22d94bdb39fc6097da9ba7a9279d5c0c
Denis <dnsbrnd@gmail.com>
2024-11-18 18:58:39 +0000

    Fixed breaking change

--- viewmodel/src/commonMain/kotlin/com/denisbrandi/androidrealca/viewmodel/ViewModel.kt
+++ viewmodel/src/commonMain/kotlin/com/denisbrandi/androidrealca/viewmodel/ViewModel.kt
@@ -13,7 +13,8 @@ interface EventViewModel<ViewEvent> {
 
 class StateDelegate<State> : StateViewModel<State> {
     private lateinit var _state: MutableStateFlow<State>
-    override val state: StateFlow<State> = _state.asStateFlow()
+    override val state: StateFlow<State>
+        get() = _state.asStateFlow()
 
     fun setDefaultState(state: State) {
         _state = MutableStateFlow(state)
