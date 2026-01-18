a78bd3caf5a43a94dcb0e74153ec88d8e9ec10f1
Denis <dnsbrnd@gmail.com>
2026-01-18 13:07:06 +0000

    Updated access token

--- httpclient/src/commonMain/kotlin/com/denisbrandi/androidrealca/httpclient/AccessTokenProvider.kt
+++ httpclient/src/commonMain/kotlin/com/denisbrandi/androidrealca/httpclient/AccessTokenProvider.kt
@@ -2,6 +2,6 @@ package com.denisbrandi.androidrealca.httpclient
 
 object AccessTokenProvider {
     fun getAccessTokenHeader(): Pair<String, String> {
-        return "Authorization" to "Bearer t98jaw2i9048ctpaxb19g901p3yaasy5xyvdwue6"
+        return "Authorization" to "Bearer sghe50o5rkn9n9n1azs0ectmxnjjls7r9i1tksz3"
     }
 }
--- httpclient/src/commonTest/kotlin/com/denisbrandi/androidrealca/httpclient/AccessTokenProviderTest.kt
+++ httpclient/src/commonTest/kotlin/com/denisbrandi/androidrealca/httpclient/AccessTokenProviderTest.kt
@@ -8,7 +8,7 @@ class AccessTokenProviderTest {
         val result = AccessTokenProvider.getAccessTokenHeader()
 
         assertEquals(
-            "Authorization" to "Bearer t98jaw2i9048ctpaxb19g901p3yaasy5xyvdwue6",
+            "Authorization" to "Bearer sghe50o5rkn9n9n1azs0ectmxnjjls7r9i1tksz3",
             result
         )
     }
